4d3463ec92a83ba9340fb9e5820432386dfc41ef
Amrit Anand Bhandari <amrbhandari@yahoo.com>
2025-10-28 14:47:03 +0530

    detect collision between bullets and enemies between ship and enemies

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
 
 using namespace std;
 
@@ -11,11 +12,19 @@ auto SPRITES_FOLDER = "images/";
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 600;
 
-int spaceshipX = 400;
-constexpr int spaceshipY = 510;
 constexpr int spaceshipWidth = 50;
+constexpr int spaceshipHeight = 80;
+constexpr int spaceshipXLeftBoundary = 10;
+constexpr int spaceshipXRightBoundary = SCREEN_WIDTH - spaceshipWidth - 10;
+int spaceshipX = (SCREEN_WIDTH - spaceshipWidth) / 2;
+constexpr int spaceshipY = 510;
 constexpr int spaceshipSpeed = 10;
+
+constexpr int bulletWidth = 2;
+constexpr int bulletHeight = 10;
+constexpr int bulletSpeed = 5;
 constexpr int maxBullets = 10;
+
 constexpr int maxEnemies = 10;
 
 SDL_Window* window = nullptr;
@@ -30,7 +39,7 @@ struct Bullet
 {
     int x;
     int y;
-    int speed;
+    int speed = bulletSpeed;
     bool active;
 };
 
@@ -44,6 +53,7 @@ struct Enemy
     int height = 20;
     int speed;
     bool active;
+    int health;
 };
 
 vector<Enemy> enemies(maxEnemies);
@@ -54,10 +64,9 @@ void fireBullets()
     {
         if (Bullet* bullet = &bullets[i]; !bullet->active)
         {
-            bullet->active = true;
-            bullet->x = spaceshipX + spaceshipWidth / 2;
+            bullet->x = spaceshipX + (spaceshipWidth / 2);
             bullet->y = spaceshipY;
-            bullet->speed = 5;
+            bullet->active = true;
 
             break;
         }
@@ -90,27 +99,119 @@ void renderBullets()
     }
 }
 
+void moveAndRenderBullets()
+{
+    for (int i = 0; i < maxBullets; i++)
+    {
+        if (Bullet* bullet = &bullets[i]; bullet->active)
+        {
+            bullet->y -= bullet->speed;
+            if (bullet->y > 0) // within the top of screen
+            {
+                SDL_Rect rect = {bullet->x - 1, bullet->y, bulletWidth, bulletHeight};
+                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red
+                SDL_RenderFillRect(renderer, &rect);
+            }
+            else
+                bullet->active = false;
+        }
+    }
+}
+
+void spawnEnemyNewPlace(Enemy& enemy)
+{
+    int x = rand() % SCREEN_WIDTH;
+    if (x + enemy.width > SCREEN_WIDTH)
+    {
+        x = SCREEN_WIDTH - enemy.width;
+    }
+
+    enemy.x = x;
+    enemy.y = rand() % 100; // - 150;
+
+    enemy.speed = rand() % 2 + 1;
+    enemy.health = rand() % 3 + 1;
+
+    enemy.active = true;
+}
+
+void initialiseEnemies()
+{
+    for (int i = 0; i < maxEnemies; i++)
+    {
+        spawnEnemyNewPlace(enemies[i]);
+    }
+}
+
+bool checkCollision(const SDL_Rect& rect1, const SDL_Rect& rect2)
+{
+    return SDL_HasIntersection(&rect1, &rect2);
+}
+
+void checkBulletsAndEnemiesCollision()
+{
+    for (int i = 0; i < maxBullets; i++)
+    {
+        if (Bullet* bullet = &bullets[i]; bullet->active)
+        {
+            SDL_Rect bulletRect = {bullet->x - 1, bullet->y, bulletWidth, bulletHeight};
+            for (int j = 0; j < maxEnemies; j++)
+            {
+                if (Enemy* enemy = &enemies[j]; enemy->active)
+                {
+                    SDL_Rect enemyRect = {enemy->x, enemy->y, enemy->width, enemy->height};
+
+                    if (checkCollision(bulletRect, enemyRect))
+                    {
+                        bullet->active = false;
+
+                        enemy->health--;
+                        enemy->active = enemy->health > 0;
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool checkShipAndEnemiesCollision(const SDL_Rect& rect)
+{
+    for (int j = 0; j < maxEnemies; j++)
+    {
+        if (Enemy* enemy = &enemies[j]; enemy->active)
+        {
+            SDL_Rect enemyRect = {enemy->x, enemy->y, enemy->width, enemy->height};
+
+            if (checkCollision(rect, enemyRect))
+            {
+                enemy->health = 0;
+                enemy->active = false;
+
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 void moveAndRenderEnemies()
 {
     for (int i = 0; i < maxEnemies; i++)
     {
-        if (Enemy* enemy = &enemies[i]; enemy->active)
+        Enemy* enemy = &enemies[i];
+        if (enemy->active)
         {
             enemy->y += enemy->speed;
-            if (enemy->y > SCREEN_HEIGHT)
-            {
-                enemy->x = rand() % SCREEN_WIDTH;
-                if (enemy->x + enemy->width >= SCREEN_WIDTH)
-                {
-                    enemy->x = SCREEN_WIDTH - enemy->width;
-                }
-                enemy->y = rand() % 100 - 150;
-                enemy->speed = rand() % 2 + 1;
-            }
-
-            SDL_Rect rect = {enemy->x, enemy->y, enemy->width, enemy->height};
-            SDL_RenderCopy(renderer, enemyTexture, nullptr, &rect);
         }
+
+        if (enemy->y > SCREEN_HEIGHT || !enemy->active)
+        {
+            spawnEnemyNewPlace(*enemy);
+        }
+
+        SDL_Rect rect = {enemy->x, enemy->y, enemy->width, enemy->height};
+        SDL_RenderCopy(renderer, enemyTexture, nullptr, &rect);
     }
 }
 
@@ -167,30 +268,17 @@ void handleEvents()
     }
 
     const Uint8* currentKeyStates = SDL_GetKeyboardState(nullptr);
-    if (currentKeyStates[SDL_SCANCODE_LEFT])
+    if (currentKeyStates[SDL_SCANCODE_LEFT]) // left arrow key
     {
-        spaceshipX = spaceshipX > 10
-                         ? spaceshipX - spaceshipSpeed
-                         : 10;
+        spaceshipX = spaceshipX > spaceshipXLeftBoundary
+                         ? spaceshipX - spaceshipSpeed // within left boundary
+                         : spaceshipXLeftBoundary;
     }
-    if (currentKeyStates[SDL_SCANCODE_RIGHT])
+    if (currentKeyStates[SDL_SCANCODE_RIGHT]) // right arrow key
     {
-        spaceshipX = (spaceshipX + spaceshipWidth < SCREEN_WIDTH - 10)
-                         ? spaceshipX + spaceshipSpeed
-                         : SCREEN_WIDTH - 10 - spaceshipWidth;
-    }
-}
-
-void initialiseEnemies()
-{
-    srand(static_cast<unsigned>(time(nullptr)));
-    for (int i = 0; i < maxEnemies; i++)
-    {
-        Enemy* enemy = &enemies[i];
-        enemy->x = rand() % SCREEN_WIDTH;
-        enemy->y = rand() % 100 - 150;
-        enemy->active = true;
-        enemy->speed = rand() % 2 + 1;
+        spaceshipX = (spaceshipX < spaceshipXRightBoundary)
+                         ? spaceshipX + spaceshipSpeed // within right boundary
+                         : spaceshipXRightBoundary;
     }
 }
 
@@ -227,6 +315,8 @@ int main()
         return 1;
     }
 
+    srand(time(nullptr));
+
     initialiseEnemies();
 
     while (!quit)
@@ -239,16 +329,20 @@ int main()
         //reset
         SDL_RenderClear(renderer);
 
-        SDL_Rect spaceshipRect = {spaceshipX, spaceshipY, spaceshipWidth, spaceshipWidth};
+        // render spaceship
+        SDL_Rect spaceshipRect = {spaceshipX, spaceshipY, spaceshipWidth, spaceshipHeight};
         SDL_RenderCopy(renderer, spaceshipTexture, nullptr, &spaceshipRect);
 
-        moveBullets();
-        renderBullets();
-
+        moveAndRenderBullets();
         moveAndRenderEnemies();
 
+        checkBulletsAndEnemiesCollision();
+        if (checkShipAndEnemiesCollision(spaceshipRect))
+        {
+            quit = true;
+        }
+
         SDL_RenderPresent(renderer);
-        SDL_Delay(1);
     }
 
     SDL_DestroyRenderer(renderer);
